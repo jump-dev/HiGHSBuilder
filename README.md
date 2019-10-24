6fe97ee582a7de3bb2a0bc4a8a962626f8fe7537
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-10-24 22:54:55 +0200

    Add autogenerated files to build highs

---
+++ README.md
@@ -0,0 +1,9 @@
+# HiGHSBuilder
+
+[![Build Status](https://travis-ci.org/matbesancon/HiGHSBuilder.svg?branch=master)](https://travis-ci.org/matbesancon/HiGHSBuilder)
+
+This repository builds binary artifacts for the highs project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/matbesancon/HiGHSBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,49 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "highs"
+version = v"0.1.0"
+
+# Collection of sources required to build highs
+sources = [
+    "https://github.com/ERGO-Code/HiGHS.git" =>
+    "989fa8ed33220badc04ca4dc22e14e59be2c4b09",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd HiGHS/
+mkdir build
+cd build
+LDFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib -Wl,-rpath-link,/opt/${target}/${target}/lib64" $LDFLAGS
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
+make
+make install
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc),
+    FreeBSD(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libhighs", :libhighs)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
